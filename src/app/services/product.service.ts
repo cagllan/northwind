a0fc28193d409b30ac798fee33c8ae197665e190
cagllan <caglanatay@windowslive.com>
2021-03-14 00:12:16 +0300

    proje

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = "https://localhost:44387/api/";
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "products/getall";
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId;
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+}
