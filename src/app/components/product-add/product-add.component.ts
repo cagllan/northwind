304d3919b5bcc1569d989ed50622ef45a9afd7cf
cagllan <caglanatay@windowslive.com>
2021-03-21 00:32:31 +0300

    product add güncellendi

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -35,11 +35,15 @@ export class ProductAddComponent implements OnInit {
     if(this.productAddForm.valid){
       let productModel = Object.assign({},this.productAddForm.value );
       this.productService.add(productModel).subscribe(response=>{
-        console.log(response);
+        
         this.toastrService.success(response.message,"Başarılı");
       }, responseError=>{
-        console.log(responseError);
-        this.toastrService.error(responseError.error);
+       if(responseError.error.Errors.length>0){
+         console.log(responseError.error.Errors);
+         for (let i = 0; i < responseError.error.Errors.length; i++) {
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama hatası");
+         }
+        }
       });
       
     }else{
