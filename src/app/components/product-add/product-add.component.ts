84f0ee4868d474c5f9b8a30445ea29c9206b6a55
cagllan <caglanatay@windowslive.com>
2021-03-20 23:06:27 +0300

    Add methodu eklendi

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, FormControl,Validators} from "@angular/forms";
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +12,11 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(
+     private formBuilder:FormBuilder,
+     private productService:ProductService,
+     private toastrService:ToastrService
+     ) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -26,7 +32,19 @@ export class ProductAddComponent implements OnInit {
   }
 
   add(){
-    
+    if(this.productAddForm.valid){
+      let productModel = Object.assign({},this.productAddForm.value );
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response);
+        this.toastrService.success(response.message,"Başarılı");
+      }, responseError=>{
+        console.log(responseError);
+        this.toastrService.error(responseError.error);
+      });
+      
+    }else{
+      this.toastrService.error("Formunuz eksik","Dikkat");
+    }
   }
 
 }
